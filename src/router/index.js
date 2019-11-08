dfd17fa05030d24b40a125359c52fe290e21830e
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2019-11-08 19:27:14 +0000

    End of first lab

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/Home'
+import Home from '@/components/Home'
+import Donations from '@/components/Donations'
+import About from '@/components/About'
+import Contact from '../components/Contact'
+import Donate from '../components/Donate'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/donations',
+      name: 'Donations',
+      component: Donations
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/donate',
+      name: 'Donate',
+      component: Donate
     }
   ]
 })
